7b118704aa21d2da7d48b48bd2fabb6b1fa26e48
igor-public <igor.asmykovich@gmail.com>
2024-04-21 19:56:57 +0200

    the update

--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -1,28 +1,25 @@
-# Source:Below code is provided by Streamlit and AWS 
-
-#1 import streamlit and chatbot file
 import streamlit as st 
-import  chatbot_backend as demo  #**Import your Chatbot file as demo
+import  chatbot_backend as test_chat  
 
-#2 Set Title for Chatbot - https://docs.streamlit.io/library/api-reference/text/st.title
-st.title("Hi, This is Chatbot Anisha :sunglasses:") # **Modify this based on the title you want in want
+# title - https://docs.streamlit.io/library/api-reference/text/st.title
+st.title("Test Chat") 
 
-#3 LangChain memory to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
+# langchain for the session State - https://docs.streamlit.io/library/api-reference/session-state
 if 'memory' not in st.session_state: 
-    st.session_state.memory = demo.demo_memory() #** Modify the import and memory function() attributes initialize the memory
+    st.session_state.memory = test_chat.memory_llm()
 
-#4 Add the UI chat history to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
-if 'chat_history' not in st.session_state: #see if the chat history hasn't been created yet
-    st.session_state.chat_history = [] #initialize the chat history
+# UI chat history to the session  https://docs.streamlit.io/library/api-reference/session-state
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
 
-#5 Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
+# show chat history - to keep the earlier messages
 for message in st.session_state.chat_history: 
     with st.chat_message(message["role"]): 
         st.markdown(message["text"]) 
 
-#6 Enter the details for chatbot input box 
+# Enter the details for chatbot input box 
      
-input_text = st.chat_input("Powered by Bedrock and LLama 2") # **display a chat input box
+input_text = st.chat_input("Chat using Bedrock + LLM") 
 if input_text: 
     
     with st.chat_message("user"): 
@@ -30,7 +27,7 @@ if input_text:
     
     st.session_state.chat_history.append({"role":"user", "text":input_text}) 
 
-    chat_response = demo.demo_conversation(input_text=input_text, memory=st.session_state.memory) #** replace with ConversationChain Method name - call the model through the supporting library
+    chat_response = test_chat.conversation_llm(input_text=input_text, memory=st.session_state.memory) #** replace with ConversationChain Method name - call the model through the supporting library
     
     with st.chat_message("assistant"): 
         st.markdown(chat_response) 
